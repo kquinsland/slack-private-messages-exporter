67516e96c030f4349900c579ff0b274e6f2bc413
Karl Quinsland <karl.quinsland@eventbrite.com>
2018-03-15 14:27:33 -0700

    ready to go

--- README.md
+++ README.md
@@ -1,2 +1,198 @@
-# slack-private-messages-exporter
-export and visualize your private slack channels and direct messages
+The Goal
+--
+
+* backup all direct messages (1:1)
+* backup all private / team chanels
+* normalize the data and present it in a useful format
+
+
+The python script in this repo _does_ support extracting public data as well, but I am going to skip this.  It's trivial for a slack admin to get the entire public export.  There are already a variety of tools that work well with the public chanel export. [Like this wonderful flask app](https://github.com/hfaran/slack-export-viewer).
+
+
+
+Thanks to github user [minniel](https://gist.github.com/minniel) for collecting the two gists that power this. I only made small ~~changes~~ hacks to the python script to handle errors and rate-limit the requests to the slack API.
+
+
+The Plan
+--
+
+There's a few parts to this whole thing.
+
+1. set up your dependencies
+2. get an API token
+3. extract everything from the slack API
+4. combine it into a useful package
+
+
+The Prep
+--
+
+Make sure you have the necessary software installed to do this.
+
+
+php version:
+```
+$:php -v
+PHP 5.6.30 (cli) (built: Oct 29 2017 20:30:32)
+Copyright (c) 1997-2016 The PHP Group
+Zend Engine v2.6.0, Copyright (c) 1998-2016 Zend Technologies
+```
+
+python version:
+```
+$:python3 --version
+Python 3.6.2
+```
+
+osx version:
+```
+$:sw_vers
+ProductName:	Mac OS X
+ProductVersion:	10.12.6
+BuildVersion:	16G1212
+```
+
+
+I've not done a whole lot of testing and i'm not going to do a whole lot of support.  I'll merge your PR's though.
+
+TL;DR:
+[![works badge](https://cdn.rawgit.com/nikku/works-on-my-machine/v0.2.0/badge.svg)](https://github.com/nikku/works-on-my-machine)
+
+
+
+
+The Doing
+--
+
+
+1. get this repo cloned
+
+```
+$ cd ~
+$ mkdir slack_exports
+$ cd slack_exports
+$ git clone git@github.com:kquinsland/slack-private-messages-exporter.git
+# or https://github.com/kquinsland/slack-private-messages-exporter.git
+```
+
+
+2. set up venv & load dependencies into venv
+
+```
+$ python3 -m venv ./venv
+(venv) $ pip3 install slacker
+```
+
+3. get an API token from here ([Legacy tokens | Slack](https://api.slack.com/custom-integrations/legacy-tokens))
+  
+
+4. run the exporter! 
+    
+    - This will take a long time!  
+      - I have ~ 3 years of slack history
+      - sack's API lets you get 100 messages at a time
+      - you can make an api call about once every second
+        - i had the best results when waiting 2 seconds per API call
+      - it took a little under 2 hours for me to get my DM, MPDM, and private chanels!
+      
+    - There is *zero* support for resuming an export!
+      - Do this when you know your computer will powered and connected to a network for a long time.
+      - Do this from a *reliable* internet connection.  I have hacked in some very basic error handeling. The export won't stop, but you'll probably have to go get the missing data yourself.
+    
+Your output will look something like this:
+
+```
+(venv) $:python3 slack_history.py --token xx-yy-zz --skipChannels
+Successfully authenticated for team YourTeam and user karl
+found 421 users
+writing metadata
+
+found private channels:
+some_project: (7 members)
+<...SNIP...>
+mpdm-person1--person2--person3-1: (3 members)
+<...SNIP...>
+some_team: (9 members)
+other-team: (7 members)
+different-team: (8 members)
+different-team-private: (9 members)
+<...SNIP...>
+getting history for private channel some_project with id XXXYYYZZZ1
+Sleeping a second to avoid rate limits....
+<...SNIP...>
+found direct messages (1:1) with the following users:
+slackbot
+<...SNIP...>
+getting history for direct messages with slackbot
+Sleeping a second to avoid rate limits....
+<...SNIP...>
+```
+
+
+5. turn the JSON into something useful.
+
+copy the json from history into working dir:
+```
+(venv) ./history$:cp -R * ../working/
+```
+
+go to working dir
+```
+(venv) ./history$:cd ../working/
+```
+
+copy users and channel info for DMs and private channels
+```
+(venv) ./working$:cp users.json channels.json direct_message/
+(venv) ./working$:cp users.json channels.json private_channels/
+```
+
+copy in the viz.php
+```
+(venv) ./working$:cp ../viz.php direct_message/
+(venv) ./working$:cp ../viz.php private_channels/
+```
+
+do the needful
+```
+(venv) ./working$:cd direct_message/
+(venv) ./direct_message$:php viz.php
+=====
+Combining JSON files for #aConversation
+=====
+..
+2 messages exported to /Users/karl.quinsland/Development/slack_exp/working/direct_message/../slack2html/json/aConversation.json
+<...SNIP...>
+```
+
+
+Move to the slack2html for DMs into something else
+```
+(venv) ./direct_message$:mv ../slack2html/ ../slack2html-DMS/
+```
+
+Switch to private channels
+```
+(venv) ./direct_message$:cd ../private_channels/
+```
+
+Repeat....
+```
+(venv) ./private_channels$:php viz.php
+=====
+Combining JSON files for #some-team
+=====
+........................................................
+3,213 messages exported to /Users/karl.quinsland/Development/slack_exp/working/private_channels/../slack2html/json/some-team.json
+<...SNIP...>
+```
+
+Just as before; rename the exported html dir to something meaningful
+```
+(venv) ./private_channels$:mv ../slack2html/ ../slack2html-private/
+
+```
+Open the index.html and you'll be good to go :)
+```
+(venv) ./private_channels$:open ../slack2html-private/html/index.html
+```
---
+++ history/slack_history.py
@@ -0,0 +1,324 @@
+from slacker import Slacker
+import json
+import argparse
+import os
+import shutil
+import copy
+from datetime import datetime
+import time
+
+# This script finds all channels, private channels and direct messages
+# that your user participates in, downloads the complete history for
+# those converations and writes each conversation out to seperate json files.
+#
+# This user centric history gathering is nice because the official slack data exporter
+# only exports public channels.
+#
+# PS, this only works if your slack team has a paid account which allows for unlimited history.
+#
+# PPS, this use of the API is blessed by Slack.
+# https://get.slack.help/hc/en-us/articles/204897248
+# " If you want to export the contents of your own private groups and direct messages
+# please see our API documentation."
+#
+# get your slack user token at the bottom of this page
+# https://api.slack.com/web
+#
+# dependencies:
+#    pip install slacker #https://github.com/os/slacker
+#
+# usage examples
+#    python slack_history.py --token='123token'
+#    python slack_history.py --token='123token' --dryRun=True
+#    python slack_history.py --token='123token' --skipDirectMessages
+#    python slack_history.py --token='123token' --skipDirectMessages --skipPrivateChannels
+
+
+# fetches the complete message history for a channel/group/im
+#
+# pageableObject could be:
+# slack.channel
+# slack.groups
+# slack.im
+#
+
+def retryloop(attempts, timeout):
+    starttime = time.time()
+    success = set()
+    for i in range(attempts): 
+        success.add(True)
+        yield success.clear
+        if success:
+            return
+        if time.time() > starttime + timeout:
+            break
+
+# channelId is the id of the channel/group/im you want to download history for.
+def getHistory(pageableObject, channelId, pageSize = 100):
+    messages = []
+    lastTimestamp = None
+
+    while(True):
+ 
+        try:
+            response = pageableObject.history(
+                channel = channelId,
+                latest  = lastTimestamp,
+                oldest  = 0,
+                count    = pageSize
+            ).body  
+
+        except Exception as e:
+            # TODO: expand this to timeout condition; can i just re-issue the pageableObject.history() call?
+            print("Something went wrong! E:{0}".format(e))
+
+        messages.extend(response['messages'])
+
+        if (response['has_more'] == True):
+            lastTimestamp = messages[-1]['ts'] # -1 means last element in a list
+            print("Sleeping a tick to avoid hitting slack's rate limits...")
+            # This might be OK at 1.  Had god results downloading several years of DMs at a value of 2
+            time.sleep(2)
+        else:
+            break
+    return messages
+
+
+def mkdir(directory):
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+
+
+# create datetime object from slack timestamp ('ts') string
+def parseTimeStamp( timeStamp ):
+    if '.' in timeStamp:
+        t_list = timeStamp.split('.')
+        if len( t_list ) != 2:
+            raise ValueError( 'Invalid time stamp' )
+        else:
+            return datetime.utcfromtimestamp( float(t_list[0]) )
+
+
+# move channel files from old directory to one with new channel name
+def channelRename( oldRoomName, newRoomName ):
+    # check if any files need to be moved
+    if not os.path.isdir( oldRoomName ):
+        return
+    mkdir( newRoomName )
+    for fileName in os.listdir( oldRoomName ):
+        shutil.move( os.path.join( oldRoomName, fileName ), newRoomName )
+    os.rmdir( oldRoomName )
+
+
+def writeMessageFile( fileName, messages ):
+    try:
+        with open(fileName, 'w') as outFile:
+            json.dump( messages, outFile, indent=4)
+    except FileNotFoundError as e:
+        # I don't know why this happen from time to time; i think it's due to channel names
+        # that might be duplicates or the API is mashing them into duplicate names
+        # we try to resecue and still save the content, though
+        print("not found: {0}! messages:{1}".format(fileName, messages))
+        fp = os.path.basename(fileName)
+        with open(fp, 'w') as oops:
+            json.dump( messages, oops, indent=4)
+    
+
+# parse messages by date
+def parseMessages( parentDir, roomDir, messages, roomType ):
+    nameChangeFlag = roomType + "_name"
+
+    currentFileDate = ''
+    currentMessages = []
+    for message in messages:
+        #first store the date of the next message
+        ts = parseTimeStamp( message['ts'] )
+        fileDate = '{:%Y-%m-%d}'.format(ts)
+
+        #if it's on a different day, write out the previous day's messages
+        if fileDate != currentFileDate:
+            outFileName = '{parent}/{room}/{file}.json'.format( parent = parentDir, room = roomDir, file = currentFileDate )
+            writeMessageFile( outFileName, currentMessages )
+            currentFileDate = fileDate
+            currentMessages = []
+
+        # check if current message is a name change
+        # dms won't have name change events
+        if roomType != "im" and ( 'subtype' in message ) and message['subtype'] == nameChangeFlag:
+            roomDir = message['name']
+            oldRoomPath = '{parent}/{room}'.format( parent = parentDir, room = message['old_name'] )
+            newRoomPath = '{parent}/{room}'.format( parent = parentDir, room = roomDir )
+            channelRename( oldRoomPath, newRoomPath )
+
+        currentMessages.append( message )
+    outFileName = '{parent}/{room}/{file}.json'.format( parent = parentDir, room = roomDir, file = currentFileDate )
+    writeMessageFile( outFileName, currentMessages )
+
+
+# fetch and write history for all public channels
+def getChannels(slack, dryRun):
+    channels = slack.channels.list().body['channels']
+
+    print("\nfound channels: ")
+    for channel in channels:
+        print(channel['name'])
+
+    if not dryRun:
+        parentDir = "channel"
+        mkdir(parentDir)
+        for channel in channels:
+            print("getting history for channel {0}".format(channel['name']))
+            channelDir = channel['name']
+            mkdir( os.path.join( parentDir, channelDir ) )
+            messages = getHistory(slack.channels, channel['id'])
+            parseMessages( parentDir, channelDir, messages, 'channel')
+
+
+# write channels.json file
+def dumpChannelFile( slack ):
+    print("Making channels file")
+    channels = slack.channels.list().body['channels']
+
+    #have to convert private channels to channels to be read in properly
+    groups = slack.groups.list().body['groups']
+    print( str(len(channels) ) )
+    for group in groups:
+        print( str(len(channels) ) )
+        new_channel = copy.copy(channels[0])
+        new_channel['id'] = group['id']
+        new_channel['name'] = group['name']
+        new_channel['created'] = group['created']
+        new_channel['creator'] = group['creator']
+        new_channel['is_archived'] = group['is_archived']
+        new_channel['is_channel'] = True
+        new_channel['is_general'] = False
+        new_channel['is_member'] = True
+        new_channel['members'] = group['members']
+        new_channel['num_members'] = len(group['members'])
+        new_channel['purpose'] = group['purpose']
+        new_channel['topic'] = group['topic']
+        channels.append( new_channel )
+
+    #We will be overwriting this file on each run.
+    with open('channels.json', 'w') as outFile:
+        json.dump( channels , outFile, indent=4)
+
+
+# fetch and write history for all direct message conversations
+# also known as IMs in the slack API.
+def getDirectMessages(slack, ownerId, userIdNameMap, dryRun):
+    dms = slack.im.list().body['ims']
+
+    print("\nfound direct messages (1:1) with the following users:")
+    for dm in dms:
+        print(userIdNameMap.get(dm['user'], dm['user'] + " (name unknown)"))
+
+    if not dryRun:
+        parentDir = "direct_message"
+        mkdir(parentDir)
+        for dm in dms:
+            name = userIdNameMap.get(dm['user'], dm['user'] + " (name unknown)")#note: double check naming of dm directory
+            print("getting history for direct messages with {0}".format(name))
+            dmDir = name
+            mkdir('{parent}/{dm}'.format( parent = parentDir, dm = dmDir ))
+            messages = getHistory(slack.im, dm['id'])
+            parseMessages( parentDir, dmDir, messages, "im" )
+
+
+# fetch and write history for all private channels
+# also known as groups in the slack API.
+def getPrivateChannels(slack, dryRun):
+    groups = slack.groups.list().body['groups']
+
+    print("\nfound private channels:")
+    for group in groups:
+        print("{0}: ({1} members)".format(group['name'], len(group['members'])))
+
+    if not dryRun:
+        parentDir = "private_channels"
+        mkdir(parentDir)
+        for group in groups:
+            messages = []
+            print("getting history for private channel {0} with id {1}".format(group['name'], group['id']))
+            groupDir = group['name']
+            mkdir( '{parent}/{group}'.format( parent = parentDir, group = groupDir ) )
+            messages = getHistory(slack.groups, group['id'])
+            parseMessages( parentDir, groupDir, messages, 'group' )
+
+# fetch all users for the channel and return a map userId -> userName
+def getUserMap(slack):
+    #get all users in the slack organization
+    users = slack.users.list().body['members']
+    userIdNameMap = {}
+    for user in users:
+        userIdNameMap[user['id']] = user['name']
+    print("found {0} users ".format(len(users)))
+    return userIdNameMap
+
+# stores json of user info
+def dumpUserFile(slack):
+    #write to user file, any existing file needs to be overwritten.
+    with open( "users.json", 'w') as userFile:
+        json.dump( slack.users.list().body['members'], userFile, indent=4 )
+
+# get basic info about the slack channel to ensure the authentication token works
+def doTestAuth(slack):
+    testAuth = slack.auth.test().body
+    teamName = testAuth['team']
+    currentUser = testAuth['user']
+    print("Successfully authenticated for team {0} and user {1} ".format(teamName, currentUser))
+    return testAuth
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='download slack history')
+
+    parser.add_argument('--token', help="an api token for a slack user")
+
+    parser.add_argument(
+        '--dryRun',
+        action='store_true',
+        default=False,
+        help="if dryRun is true, don't fetch/write history only get channel names")
+
+    parser.add_argument(
+        '--skipPrivateChannels',
+        action='store_true',
+        default=False,
+        help="skip fetching history for private channels")
+
+    parser.add_argument(
+        '--skipChannels',
+        action='store_true',
+        default=False,
+        help="skip fetching history for channels")
+
+    parser.add_argument(
+        '--skipDirectMessages',
+        action='store_true',
+        default=False,
+        help="skip fetching history for directMessages")
+
+    args = parser.parse_args()
+
+    slack = Slacker(args.token)
+
+    testAuth = doTestAuth(slack)
+
+    userIdNameMap = getUserMap(slack)
+
+    dryRun = args.dryRun
+
+    if not dryRun:
+        #write channel and user jsons
+        dumpUserFile(slack)
+        dumpChannelFile(slack)
+
+    if not args.skipChannels:
+        getChannels(slack, dryRun)
+
+    if not args.skipPrivateChannels:
+        getPrivateChannels(slack, dryRun)
+
+    if not args.skipDirectMessages:
+        getDirectMessages(slack, testAuth['user_id'], userIdNameMap, dryRun)
+
